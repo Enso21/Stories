35accf5d0a17666d9caffcc3fad38c342edcd5df
Enso21 <miscellaneousinc15@gmail.com>
2023-05-02 11:07:08 -0400

    Working transitions
    - Add container which transitions when changing content pages.
    - Basic GSAP timeline to expand the container to cover the entire div
    - Use correct barba script

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,44 @@
-import barba from '@barba/core';
-import { gsap } from "gsap";
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to(".load-screen", {
+    duration: 1.2,
+    width: "100%",
+    left: "0%",
+    ease: "Expo.easeInOut",
+  });
+
+  tl.to(".load-screen", {
+    duration: 1,
+    width: "100%",
+    left: "100%",
+    ease: "Expo.easeInOut",
+    delay: 0.3,
+  });
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+
+barba.init({
+  sync: true,
+
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+
+        pageTransition();
+        await delay(1000);
+        done();
+      },
+    },
+  ],
+});
